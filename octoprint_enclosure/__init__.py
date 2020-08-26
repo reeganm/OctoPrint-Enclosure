6c5926113cf3325b08ba97ded0963746cc5c054e
Reegan McAllister <mcallisterreegan@gmail.com>
2020-08-26 14:00:55 -0600

    load settings on startup instead of after

--- octoprint_enclosure/__init__.py
+++ octoprint_enclosure/__init__.py
@@ -104,13 +104,15 @@ class EnclosurePlugin(octoprint.plugin.StartupPlugin, octoprint.plugin.TemplateP
         return -1
 
     # ~~ StartupPlugin mixin
+    def on_startup(self):
+        self.rpi_outputs = self._settings.get(["rpi_outputs"])
+        self.rpi_inputs = self._settings.get(["rpi_inputs"])
+        self.notifications = self._settings.get(["notifications"])
+
     def on_after_startup(self):
         self.pwm_instances = []
         self.event_queue = []
         self.rpi_outputs_not_changed = []
-        self.rpi_outputs = self._settings.get(["rpi_outputs"])
-        self.rpi_inputs = self._settings.get(["rpi_inputs"])
-        self.notifications = self._settings.get(["notifications"])
         self.generate_temp_hum_control_status()
         self.setup_gpio()
         self.configure_gpio()
